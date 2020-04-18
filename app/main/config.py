c80e18e2a124cd2142f0df4f176da2e6cd6cd467
Luifer Villalba <villalba.luifer@gmail.com>
2020-04-18 17:59:09 -0400

    Added API until now

--- app/main/config.py
+++ app/main/config.py
@@ -1,20 +1,20 @@
 import os
 
 # uncomment the line below for postgres database url from environment variable
-# postgres_local_base = os.environ['DATABASE_URL']
+postgres_local_base = os.environ['DATABASE_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'G1gmyTb8A8')
     DEBUG = False
 
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
-    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_main.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
---
+++ app/main/util/dto.py
@@ -0,0 +1,11 @@
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    api = Namespace('user', description='User related operations')
+    user = api.model('user', {
+        'email': fields.String(required=True, description='User Email address'),
+        'username': fields.String(required=True, description='User Username'),
+        'password': fields.String(required=True, description='User Password'),
+        'public_id': fields.String(description='User Identifier')
+    })
